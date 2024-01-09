ad0778926f218094ca0750adfa30de2cc84d05fb
Comeng18 <contact@emrecogalan.com>
2024-01-09 23:51:05 +0300

    Component, props, state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React from "react";
+import Navi from "./Navi";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
+import { Container, Row, Col } from "reactstrap";
 
 export default function App() {
+  let productInfo = { title: "Product List" };
+  let categoryInfo = { title: "Category List" };
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Container>
+        <Row>
+          <Navi />
+        </Row>
+        <Row>
+          <Col xs="3">
+            <CategoryList info={categoryInfo} />
+          </Col>
+          <Col xs="9">
+            <ProductList info={productInfo} />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
